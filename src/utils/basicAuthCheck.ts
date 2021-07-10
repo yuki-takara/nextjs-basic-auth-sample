2bc15c52546f09a564f6ec448758a00317f1fa93
yuki-takara <tkra.yuuki@gmail.com>
2021-07-10 09:27:59 +0900

    feat: BASIC認証でキャンセルをした場合に正常系のページを表示しない

--- src/utils/basicAuthCheck.ts
+++ src/utils/basicAuthCheck.ts
@@ -1,8 +1,18 @@
 import initializeBasicAuth from 'nextjs-basic-auth'
+import type { IncomingMessage, ServerResponse } from 'http'
 
 const users = [
   { user: 'user', password: 'password' },
   { user: 'admin', password: 'admin' },
 ]
 
-export default initializeBasicAuth({ users })
+const basicAuthCheck = async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
+  await initializeBasicAuth({ users })(req, res)
+
+  // Workaround for major bug: If you cancel, the page loads normally. - https://github.com/jchiatt/nextjs-basic-auth/issues/4
+  if (res.statusCode === 401) {
+    res.end('<html>Unauthorized</html>')
+  }
+}
+
+export default basicAuthCheck
